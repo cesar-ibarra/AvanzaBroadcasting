f0c679add6d9f181f3217588f63c82a53388be66
Cesar Ibarra <ibarratcesar@gmail.com>
2022-03-05 16:09:09 -0800

    Add Lottie Package

---
+++ AvanzaBroadcasting/LottieView.swift
@@ -0,0 +1,40 @@
+//
+//  LottieView.swift
+//  Lottie-Demo
+//
+//  Created by Cesar Ibarra on 3/2/22.
+//
+
+import SwiftUI
+import Lottie
+
+struct LottieView: UIViewRepresentable {
+    typealias UIViewType = UIView
+    
+    var filename: String
+    
+    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> UIView {
+        let view = UIView(frame: .zero)
+        
+        let animationView = AnimationView()
+        let animation = Animation.named(filename)
+        animationView.animation = animation
+        animationView.contentMode = .scaleAspectFit
+        animationView.play()
+        
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(animationView)
+        
+        NSLayoutConstraint.activate([
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor)
+        ])
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<LottieView>) {
+        
+    }
+
+}
